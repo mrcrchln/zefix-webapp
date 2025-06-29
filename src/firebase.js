6e5450d7fe491b1fa1dffc827a0a5297f5380c3d
mrcrchln <124362563+mrcrchln@users.noreply.github.com>
2025-06-29 16:53:53 +0100

    Initial commit: React + Tailwind + Firebase project setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMmJcU61x0OeHUnPUQ0ydqugoGJBZYNs8",
+  authDomain: "zefix-app-sge.firebaseapp.com",
+  projectId: "zefix-app-sge",
+  storageBucket: "zefix-app-sge.firebasestorage.app",
+  messagingSenderId: "346735712094",
+  appId: "1:346735712094:web:48271fb7e43abd964121cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
